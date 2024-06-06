01ada9a5a6184135105245acf7b2df9240db70cc
Richard Go <richard.s.go@outlook.com>
2024-06-05 19:48:17 -0700

    Last comment out is index.ts/Constructor bec it was giving me error on packaging the solution using 'msbuild /t:build /restore' vs installer command prompt.Add project files.

---
+++ generated/ManifestTypes.d.ts
@@ -0,0 +1,19 @@
+/*
+*This is auto generated from the ControlManifest.Input.xml file
+*/
+
+// Define IInputs and IOutputs Type. They should match with ControlManifest.
+export interface IInputs {
+    GlowText: ComponentFramework.PropertyTypes.StringProperty;
+    TextSize: ComponentFramework.PropertyTypes.StringProperty;
+    TextColor: ComponentFramework.PropertyTypes.StringProperty;
+    TextAlign: ComponentFramework.PropertyTypes.StringProperty;
+    TextWeight: ComponentFramework.PropertyTypes.StringProperty;
+}
+export interface IOutputs {
+    GlowText?: string;
+    TextSize?: string;
+    TextColor?: string;
+    TextAlign?: string;
+    TextWeight?: string;
+}
